87ec6dac3dbb40887242ed375725c051b64b8fff
Maciej Olejnik <maciekolej@gmail.com>
2018-03-28 17:30:04 +0200

    Client RTC

--- src/client/client.js
+++ src/client/client.js
@@ -1,24 +1,180 @@
 const webrtc = require('webrtc-adapter');
 const io = require('socket.io-client');
+const adapter = require('webrtc-adapter');
 
 class Client {
     constructor() {
+        console.log("Client creating...");
+
+        this.myUsername = Math.random();
+        this.partnerUsername = null;
+
+        console.log("Client username: " + this.myUsername);
+
+        this.createIO();
+        this.createRTC();
+
+    }
+
+    parseMessageIO(message)
+    {
+        console.log("Message from server:", message);
+
+        switch(message.cmd)
+        {
+            case "welcome":
+            {
+                /*
+                {
+                    cmd: "welcome",
+                    motd: "Hello from the server!",
+                    username: "your_username"
+                }
+                */
+
+                console.log(message.motd);
+                this.myUsername = message.username;
+
+                break;
+            }
+
+            case "candidate":
+            {
+                /*
+                {
+                    cmd: "candidate",
+                    data: {...} // ice object
+                }
+                */
+
+                this.RTCConnection.addIceCandidate(new RTCIceCandidate(message.data)); 
+                
+                break;
+            }
+
+            case "match":
+            {
+                /*
+                {
+                    cmd: "match",
+                    partnerUsername: "someone_nick",
+                    whoCaller: "someone_nick" // or "my_nick"
+                }
+                */
+
+                this.partnerUsername = message.partnerUsername;
+
+                // Who start
+                if(message.whoCaller == this.myUsername)
+                {
+                    // Create offer message
+                    this.RTCConnection.createOffer((offer) => {
+                        let offerMessage = {
+                            cmd: "offer",
+                            data: offer
+                        }
+
+                        // Server must check for what user is that offer (from the match object)
+                        this.socket.emit('send', offerMessage);
+
+                        this.RTCConnection.setLocalDescription(offer);
+                    });
+                }
+
+                break;
+            }
+
+            case "offer":
+            {
+                /*
+                {
+                    cmd: "offer",
+                    data: {...} // offer object
+                }
+                */
+
+                this.RTCConnection.setRemoteDescription(new RTCSessionDescription(message.data));
+
+                this.RTCConnection.createAnswer((answer) => {
+
+                    this.RTCConnection.setLocalDescription(answer);
+
+                    let answerMessage = {
+                        cmd: "answer",
+                        data: answer
+                    }
+
+                    // Server must check for what user is that answer (from the match object)
+                    this.socket.emit('send', answerMessage);
+
+                });
+
+                break;
+            }
+
+            case "answer":
+            {
+                /*
+                {
+                    cmd: "answer",
+                    data: {...} // offer object
+                }
+                */
+
+                this.RTCConnection.setRemoteDescription(new RTCSessionDescription(message.data)); 
+
+                break;
+            }
+        }
+    }
+
+    iceCandidateHandler(event)
+    {
+        if (event.candidate) { 
+            // Create candidate message
+            let candidateMessage = {
+                cmd: "candidate",
+                data: event.candidate
+            }
+
+            this.socket.emit('send', candidateMessage);
+         } 
+
+         // todo: on server - check for what user is that event 
+    }
+
+    createIO()
+    {
+        console.log("Creating IO connection...");
+
         this.socket = io.connect('http://localhost:3000');
 
-        this.socket.emit('send', { message: "PING" });
-        console.log("Client send: PING");
+        // Create init message
+        let initMessage = {
+            cmd: "welcome"
+        }
 
-        this.socket.on('message', function (data) {
-            console.log("Message from server:", data.message);
-        });
+        this.socket.emit('send', initMessage);
 
-        //this.connection = new RTCPeerConnection();
+        // Subscribe messages from the server
+        this.socket.on('message', this.parseMessageIO);
+    }
 
+    createRTC()
+    {
+        let configuration = { 
+            //"iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
+        }; 
+
+        this.RTCConnection = RTCPeerConnection(configuration);
+
+        //todo AddStream to connection
+
+        this.RTCConnection.onicecandidate = this.iceCandidateHandler;
     }
 }
 
 
 window.onload = () => {
-    console.log("HELLO FROM THE CLIENT!");
     let client = new Client();
 };
